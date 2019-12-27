33bf5710e509e072e22de019d8ebf24a887681c4
Mark de Does <mark@mdedoes.xs4all.nl>
2019-12-27 12:00:20 +0100

    - Accidentally added generated files

--- docField/docFieldConfig.h
+++
@@ -1,8 +0,0 @@
-/* docFieldConfig.h.  Generated from docFieldConfig.h.in by configure.  */
-#   ifndef	DOC_FIELD_CONFIG_H
-#   define	DOC_FIELD_CONFIG_H
-
-/* Define if you have the ANSI C header files.  */
-#define STDC_HEADERS 1
-
-#   endif	/*  DOC_FIELD_CONFIG_H  */
